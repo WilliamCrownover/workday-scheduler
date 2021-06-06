048f9293b7055214916aa36401ddab81ad4c978a
William Crownover <williamcrownover1@gmail.com>
2021-06-05 18:32:48 -0700

    Developing the template literal to load the hour rows to the page

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // GLOBAL VARIABLES
 // Elements
 var todaysDateEl = $("#currentDay");
+var hourBlockContainerEl = $("#hourBlockContainer");
 
 // Tracking data
 var date;
@@ -13,8 +14,27 @@ function currentTime() {
     todaysDateEl.text(date);
 }
 
+function hourPasPreFut(hourIndex) {
+    var currentHour = moment().format("H")
+    return (hourIndex < currentHour ? "past" : (hourIndex > currentHour ? "future" : "present"));
+}
+
 // ---------------------------------------------------------------------------------------------------
 
 // Update time to present immediately and every second
 currentTime();
 var updatingTimeInterval = setInterval(currentTime, 1000);
+
+for(var i = 0; i < 9; i++) {
+    hourBlockContainerEl.append(`
+        <div class="row">
+            <div class="col-1 hour">
+                <p>${moment(i+9,"H").format("h a")}</p>
+            </div>
+            <textarea class="col-10 ${hourPasPreFut(i+11)}"></textarea>
+            <div class="col-1 saveBtn">
+                <i class="fas fa-save fa-2x"></i>
+            </div>
+        </div>
+    `);
+}
